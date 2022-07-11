a8482e830cacb030a699abe4995076d0433be399
Parzivalsg <89161666+Parzivalsg@users.noreply.github.com>
2022-07-11 10:14:03 +0530

    Update Day 3--5)Sliding_Window_Maximum.cpp

--- Day 3--5)Sliding_Window_Maximum.cpp
+++ Day 3--5)Sliding_Window_Maximum.cpp
@@ -6,18 +6,9 @@ public:
         int i=0,j=0;
         while(j<nums.size())
         {
-            if(check.empty() or nums[j]>check.front())
-            {
-                while(!check.empty())
-                    check.pop_back();
-                check.push_back(nums[j]);
-            }
-            else
-            {
-                while(nums[j]>check.back())
-                    check.pop_back();
-                check.push_back(nums[j]);
-            }
+            while(!check.empty() and nums[j]>check.back())
+                check.pop_back();
+            check.push_back(nums[j]);
             if(j-i+1==k)
             {
                 ans.push_back(check.front());
